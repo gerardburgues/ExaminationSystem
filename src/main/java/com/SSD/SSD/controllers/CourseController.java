b7ced92f3ec3790d708bc83b46f6cb4bfd4175aa
GerardBurgues <gerard61999@gmail.com>
2021-06-13 16:02:40 +0200

    nothing

--- src/main/java/com/SSD/SSD/controllers/CourseController.java
+++ src/main/java/com/SSD/SSD/controllers/CourseController.java
@@ -1,26 +1,44 @@
 package com.SSD.SSD.controllers;
 
 import com.SSD.SSD.model.Course;
+import com.SSD.SSD.model.StudentCourse;
 import com.SSD.SSD.services.CourseService;
+import com.SSD.SSD.services.StudentCourseService;
 import com.SSD.SSD.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "course")
 public class CourseController {
 
     private final CourseService courseService;
+    private final StudentCourseService studentCourseService;
     private final StudentService studentService;
 
     @Autowired
-    public CourseController(CourseService courseService, StudentService studentService) {
+    public CourseController(CourseService courseService, StudentCourseService studentCourseService, StudentService studentService) {
         this.courseService = courseService;
+        this.studentCourseService = studentCourseService;
         this.studentService = studentService;
     }
+
+    @GetMapping("/showNewStudentForm")
+    public String showNewStudentForm(Model model){
+
+        StudentCourse studentCourse = new StudentCourse();
+        model.addAttribute("studentcourse", studentCourse);
+        return "new_student";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") StudentCourse studentCourse) {
+        // save employee to database
+        studentCourseService.saveStudentCourse(studentCourse);
+        return "redirect:/";
+    }
+
 
     @GetMapping
     public String displayCourse (Model model){
